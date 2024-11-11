5f5c79a6d3c5a1251fc091b4a05c3ce44f0c90f5
office_pc\magic <phuvh@vng.com.vn>
2024-11-11 10:19:27 +0700

    use class-based for subcribe flux

--- src/main/kotlin/p/k/main.kt
+++ src/main/kotlin/p/k/main.kt
@@ -6,6 +6,7 @@ import io.r2dbc.postgresql.PostgresqlConnectionFactoryProvider
 import io.r2dbc.spi.ConnectionFactories
 import io.r2dbc.spi.ConnectionFactoryOptions.*
 import kotlinx.coroutines.CompletableDeferred
+import kotlinx.coroutines.channels.SendChannel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.channelFlow
 import kotlinx.coroutines.flow.map
@@ -51,30 +52,12 @@ suspend fun main(vararg args: String) {
     val rSelect = dsl
         .select() // select all
         .from(g)
-        .limit(1)
+        .limit(2)
     val flux = Flux.from(rSelect)
     val allRecord = channelFlow<Record> {
-        val wait = CompletableDeferred<Unit>()
-        flux.subscribe(object : Subscriber<Record> {
-            override fun onSubscribe(s: Subscription) {
-                s.request(Long.MAX_VALUE)
-            }
-
-            override fun onNext(t: Record) {
-                // block send ????
-                trySend(t)
-            }
-
-            override fun onError(t: Throwable) {
-                wait.completeExceptionally(t)
-            }
-
-            override fun onComplete() {
-                wait.complete(Unit)
-            }
-        })
-        // need wait to publisher emit all the data
-        wait.await()
+        val sub = SubscriberForChanel(this)
+        flux.subscribe(sub)
+        sub.wait.await()
     }
     logger.info("done create flow")
     allRecord.collect {
@@ -86,6 +69,27 @@ suspend fun main(vararg args: String) {
 
 }
 
+
+class SubscriberForChanel<T>(
+    private val sendChannel: SendChannel<T>,
+    val wait: CompletableDeferred<Unit> = CompletableDeferred(),
+) : Subscriber<T> {
+    override fun onSubscribe(s: Subscription) {
+        s.request(Int.MAX_VALUE.toLong())
+    }
+
+    override fun onNext(t: T) {
+        sendChannel.trySend(t)
+    }
+
+    override fun onError(t: Throwable) {
+        wait.completeExceptionally(t)
+    }
+
+    override fun onComplete() {
+        wait.complete(Unit)
+    }
+}
 
 class CCC(private var i: Int = 0) : suspend (Any?) -> Int {
     override suspend fun invoke(p1: Any?): Int {
